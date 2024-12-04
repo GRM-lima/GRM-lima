81add9e44d1715b8c6bdd2d8f32709ab17a2bc94
GRM <rafaelcarvalhodelima98@gmail.com>
2024-12-03 22:12:05 -0300

    03.12.2024

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.get('/:sigla', (requisicao, resposta) => {
     const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada);// Busca o carro pela Sigla
     if (!carro) {
         // ! Vazio Not
+        // Se o carro nao for encontrado, retorna erro 404
         resposta
         .status(404)
         .send(
@@ -30,8 +31,45 @@ app.post('/', (req, res) => {
     res.status(200).send(novoCarro); //Retorna o carro adicionado com status 200 (Ok).
 });
 
+
+app.put('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase(); // Obtem a sigla
+    const carroSelecionado = carros2024.find(c => c.sigla === siglaInformada); // Busca o carro pela sigla
+    if (!carroSelecionado) {
+        res
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada!' // Mensagem de erro
+        );
+        return;
+    };
+    const campos = Object.keys(req.body); // Obtém o corpo da requisição enviada
+    for (let campo of campos) {
+        carroSelecionado[campo] = req.body[campo]; // Atualiza o carro com a informação
+    }
+    res.status(200).send(carroSelecionado); // Retorna a lista atualizada
+});
+
+app.delete('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase(); // Obtem a sigla
+    const IndiceCarroSelecionado = carros2024.findIndex(
+        (c) => c.sigla === siglaInformada // Busca o índice do carro na lista
+    );
+    if ( IndiceCarroSelecionado === -1) {
+        // Se o carro não for encontrado/indice retorna -1
+        res
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada!' // Mensagem de erro
+        );
+    return;
+    };
+    const carroRemovido = carros2024.splice(IndiceCarroSelecionado, 1); // Remove o carro
+    res.status(200).send(carroRemovido); // Retorna o carro removido com OK
+});
+
 // Inicia o servidor na porta 3000:
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
 
 // node app.js
-//localhost:3000/
+// localhost:3000/
